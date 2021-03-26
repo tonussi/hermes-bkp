1abd33df7cdb8ebf65139b5b84a31dd9bbca93c9
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2021-03-26 20:40:36 -0300

    adds result for the second scenario

--- scripts/collect-logs.sh
+++ scripts/collect-logs.sh
@@ -2,9 +2,10 @@
 KUBERNETES_DIR=$1
 SCENE=$2
 TEST=$3
+APP=tcp-kv-hashicorp-raft-leader
 
 echo "collecting throughput log..."
-kubectl cp $(kubectl get pods -l app=tcp-kv-server -o=jsonpath='{.items[0].metadata.name}'):/tmp/throughput.log logs/$SCENE/throughput/$TEST.log
+kubectl cp $(kubectl get pods -l app=$APP -o=jsonpath='{.items[0].metadata.name}'):/tmp/throughput.log logs/$SCENE/throughput/$TEST.log
 
 echo "collecting latency log..."
 kubectl logs $(kubectl get pods -l app=tcp-kv-client -o=jsonpath='{.items[0].metadata.name}') > logs/$SCENE/latency/$TEST.log
@@ -13,4 +14,5 @@ echo "deleting client..."
 kubectl delete -f $KUBERNETES_DIR/tcp-kv-client.yml
 
 echo "deleting server..."
-kubectl delete -f $KUBERNETES_DIR/tcp-kv-server.yml
+kubectl delete -f $KUBERNETES_DIR/$APP.yml
+kubectl delete -f $KUBERNETES_DIR/tcp-kv-hashicorp-raft-followers.yml
