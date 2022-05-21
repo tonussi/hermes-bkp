0d7d422cd0c814a01fdb10bdeb435a0a77a6ef45
Lucas <lucas@prevision.com.br>
2022-05-21 18:40:21 -0300

    Weep

--- scripts/mini-cluster.sh
+++ scripts/mini-cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+MINIKUBE_NODE_PREFIX=$1
 #  minikube start --nodes 3 -p 10.10.1.2 10.10.1.1 10.10.1.3
-kubectl label nodes multinode-demo kubernetes.io/role=landowner --overwrite
-kubectl label nodes multinode-demo-m02 kubernetes.io/role=hardworker --overwrite
-kubectl label nodes multinode-demo-m03 kubernetes.io/role=hardworker --overwrite
+kubectl label nodes $MINIKUBE_NODE_PREFIX kubernetes.io/role=landowner --overwrite
+kubectl label nodes $MINIKUBE_NODE_PREFIX-m02 kubernetes.io/role=hardworker --overwrite
+kubectl label nodes $MINIKUBE_NODE_PREFIX-m03 kubernetes.io/role=hardworker --overwrite
 kubectl get nodes
