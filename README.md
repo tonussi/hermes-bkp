e2f59dcb042bf2796486b20f9d0d5912589c1864
Lucas <lucas@prevision.com.br>
2022-05-30 01:00:40 -0300

    Add

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ https://www.digitalocean.com/community/tutorials/how-to-create-a-kubernetes-clus
 # Creating Distributed KV Database by Implementing Raft Consensus Using Golang
 
 https://yusufs.medium.com/creating-distributed-kv-database-by-implementing-raft-consensus-using-golang-d0884eef2e28
+
+# How To Access Gke Kubectl Proxy Dashboard
+
+https://stackoverflow.com/a/67411808
--- scripts/testing/e4.sh
+++ scripts/testing/e4.sh
@@ -47,17 +47,25 @@ echo "apply clients..."
 envsubst < $KUBERNETES_DIR/http-log-client.yml | kubectl apply -f -
 
 echo "wait job to complete..."
-kubectl wait --for=condition=complete --timeout=360s job.batch/http-log-client
+kubectl wait --for=condition=complete --timeout=1h job.batch/http-log-client
 
 TEST=$(expr $N_CLIENTS \* $N_THREADS)-$N_CLIENTS
 
 echo "collecting throughput log..."
-kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/throughput.log logs/lucas/$SCENE/throughput/$TEST.log
-kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/operations.log logs/lucas/$SCENE/operations/$TEST.log
+kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/throughput.log logs/$SCENE/throughput/leader/$TEST.log
+kubectl cp $(kubectl get pods -l app=hermes-follower -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/throughput.log logs/$SCENE/throughput/follower-0/$TEST.log
+kubectl cp $(kubectl get pods -l app=hermes-follower -o=jsonpath='{.items[1].metadata.name}'):/tmp/logs/throughput.log logs/$SCENE/throughput/follower-1/$TEST.log
+
+kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/operations.log logs/$SCENE/operations/data-leader/$TEST.log
+kubectl cp $(kubectl get pods -l app=hermes-follower -o=jsonpath='{.items[0].metadata.name}'):/tmp/logs/operations.log logs/$SCENE/operations/data-follower-0/$TEST.log
+kubectl cp $(kubectl get pods -l app=hermes-follower -o=jsonpath='{.items[1].metadata.name}'):/tmp/logs/operations.log logs/$SCENE/operations/data-follower-1/TEST.log
 
 echo "collecting latency log..."
-mkdir -p logs/lucas/$SCENE/latency
-kubectl logs $(kubectl get pods -l app=http-log-client -o=jsonpath='{.items[0].metadata.name}') > logs/lucas/$SCENE/latency/$TEST.log
+for i in $(seq $(expr $N_CLIENTS - 1))
+do
+  mkdir -p logs/$SCENE/latency/client-$i
+  kubectl logs $(kubectl get pods -l app=http-log-client -o=jsonpath='{.items[0].metadata.name}') > logs/$SCENE/latency/client-$i/$TEST.log
+done
 
 echo "deleting client..."
 kubectl delete -f $KUBERNETES_DIR/http-log-client.yml
