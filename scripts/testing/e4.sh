3ce253d55700597d770d7eb2c8a1aecdd5766e81
Lucas <lucas@prevision.com.br>
2022-06-05 01:40:10 -0300

    Add

--- scripts/testing/e4.sh
+++ scripts/testing/e4.sh
@@ -12,97 +12,100 @@ export SERVICE_NAME=hermes-leader
 
 TEST="QTYCLI"$(expr $N_CLIENTS \* $N_THREADS)
 EXPERIMENT_NAME=$SCENE/"R"$READ_RATE"W"$(expr 100 \- $READ_RATE)/$TEST
-mkdir -p logs/$EXPERIMENT_NAME/throughput
-mkdir -p logs/$EXPERIMENT_NAME/logs
-mkdir -p logs/$EXPERIMENT_NAME/operations
-mkdir -p logs/$EXPERIMENT_NAME/operations
-mkdir -p logs/$EXPERIMENT_NAME/latency
+# mkdir -p logs/$EXPERIMENT_NAME/throughput
+# mkdir -p logs/$EXPERIMENT_NAME/outputs
+# mkdir -p logs/$EXPERIMENT_NAME/operations
+# mkdir -p logs/$EXPERIMENT_NAME/latency
 
-echo "apply leader..."
-kubectl apply -f $KUBERNETES_DIR/hermes-leader.yml
+# echo "apply leader..."
+# kubectl apply -f $KUBERNETES_DIR/hermes-leader.yml
 
-sleep 5
+# sleep 5
 
-echo "wait all replicas to be ready..."
-until [ "$(kubectl get deployments -l app=hermes-leader -o jsonpath="{.items[0].status.replicas}")" = "$(kubectl get deployments -l app=hermes-leader -o jsonpath="{.items[0].status.readyReplicas}")" ]
-do
-sleep 5;
-done
+# echo "wait all replicas to be ready..."
+# until [ "$(kubectl get deployments -l app=hermes-leader -o jsonpath="{.items[0].status.replicas}")" = "$(kubectl get deployments -l app=hermes-leader -o jsonpath="{.items[0].status.readyReplicas}")" ]
+# do
+# sleep 5;
+# done
 
-echo "wait server to be running..."
-until [ "$(kubectl get pods -l app=hermes-leader -o jsonpath="{.items[0].status.phase}")" = "Running" ]
-do
-sleep 5;
-done
+# echo "wait server to be running..."
+# until [ "$(kubectl get pods -l app=hermes-leader -o jsonpath="{.items[0].status.phase}")" = "Running" ]
+# do
+# sleep 5;
+# done
 
-echo "apply followers..."
-kubectl apply -f $KUBERNETES_DIR/hermes-followers.yml
+# echo "apply followers..."
+# kubectl apply -f $KUBERNETES_DIR/hermes-followers.yml
 
-sleep 10
+# sleep 10
 
-echo "wait all replicas to be ready..."
-until [ "$(kubectl get deployments -l app=hermes-followers -o jsonpath="{.items[0].status.replicas}")" = "$(kubectl get deployments -l app=hermes-followers -o jsonpath="{.items[0].status.readyReplicas}")" ]
-do
-sleep 5;
-done
+# echo "wait all replicas to be ready..."
+# until [ "$(kubectl get deployments -l app=hermes-followers -o jsonpath="{.items[0].status.replicas}")" = "$(kubectl get deployments -l app=hermes-followers -o jsonpath="{.items[0].status.readyReplicas}")" ]
+# do
+# sleep 5;
+# done
 
-echo "wait server to be running..."
-until [ "$(kubectl get pods -l app=hermes-followers -o jsonpath="{.items[0].status.phase}")" = "Running" ]
-do
-sleep 5;
-done
+# echo "wait server to be running..."
+# until [ "$(kubectl get pods -l app=hermes-followers -o jsonpath="{.items[0].status.phase}")" = "Running" ]
+# do
+# sleep 5;
+# done
 
-sleep 10
+# sleep 10
 
-echo "apply clients..."
-envsubst < $KUBERNETES_DIR/http-log-client.yml | kubectl apply -f -
+# echo "apply clients..."
+# envsubst < $KUBERNETES_DIR/http-log-client.yml | kubectl apply -f -
 
-echo "wait job to complete..."
-kubectl wait --for=condition=complete --timeout=100s job.batch/http-log-client
+# echo "wait job to complete..."
+# kubectl wait --for=condition=complete --timeout=100s job.batch/http-log-client
 
-echo "collecting hermes throughput..."
-echo $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}')
-kubectl logs $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}') http-log-server > logs/$EXPERIMENT_NAME/throughput/leader.log
+# echo "collecting hermes throughput..."
+# echo $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}')
+# kubectl logs $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}') http-log-server > logs/$EXPERIMENT_NAME/throughput/leader.log
 
-for i in $(seq 0 1)
-do
-echo "collecting followers throughput..."
-echo $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}")
-kubectl logs $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}") http-log-server > logs/$EXPERIMENT_NAME/throughput/follower-$i.log
-done
+# for i in $(seq 0 1)
+# do
+# echo "collecting followers throughput..."
+# echo $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}")
+# kubectl logs $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}") http-log-server > logs/$EXPERIMENT_NAME/throughput/follower-$i.log
+# done
 
-echo "collecting hermes logs..."
-echo $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}')
-kubectl logs $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}') hermes > logs/$EXPERIMENT_NAME/logs/leader.log
+# echo "collecting hermes logs..."
+# echo $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}')
+# kubectl logs $(kubectl get pods -l app=hermes-leader -o=jsonpath='{.items[0].metadata.name}') hermes > logs/$EXPERIMENT_NAME/outputs/leader.log
 
-for i in $(seq 0 1)
-do
-echo "collecting followers logs..."
-echo $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}")
-kubectl logs $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}") hermes > logs/$EXPERIMENT_NAME/logs/follower-$i.log
-done
+# for i in $(seq 0 1)
+# do
+# echo "collecting followers logs..."
+# echo $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}")
+# kubectl logs $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}") hermes > logs/$EXPERIMENT_NAME/outputs/follower-$i.log
+# done
 
-echo "collecting data..."
-# Defaulted container "http-log-server" out of: http-log-server, hermes
-echo $(kubectl get pods -l app=hermes-leader -o=jsonpath="{.items[0].metadata.name}")
-kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath="{.items[0].metadata.name}"):/tmp/logs/operations.log logs/$EXPERIMENT_NAME/operations/leader.log
+# echo "collecting data..."
+# # Defaulted container "http-log-server" out of: http-log-server, hermes
+# echo $(kubectl get pods -l app=hermes-leader -o=jsonpath="{.items[0].metadata.name}")
+# kubectl cp $(kubectl get pods -l app=hermes-leader -o=jsonpath="{.items[0].metadata.name}"):/tmp/logs/operations.log logs/$EXPERIMENT_NAME/operations/leader.log
 
-for i in $(seq 0 1)
-do
-echo "collecting data..."
-kubectl cp $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}"):/tmp/logs/operations.log logs/$EXPERIMENT_NAME/operations/follower-$i.log
-done
+# for i in $(seq 0 1)
+# do
+# echo "collecting data..."
+# kubectl cp $(kubectl get pods -l app=hermes-followers -o=jsonpath="{.items[$i].metadata.name}"):/tmp/logs/operations.log logs/$EXPERIMENT_NAME/operations/follower-$i.log
+# done
 
-echo "collecting latency log..."
-for i in $(seq $(expr $N_CLIENTS - 1))
-do
-echo $(kubectl get pods -l app=http-log-client -o=jsonpath="{.items[$i].metadata.name}")
-kubectl logs $(kubectl get pods -l app=http-log-client -o=jsonpath="{.items[$i].metadata.name}") > logs/$EXPERIMENT_NAME/latency/client-$i.log
-done
+# echo "collecting latency log..."
+# for i in $(seq $(expr $N_CLIENTS - 1))
+# do
+# echo $(kubectl get pods -l app=http-log-client -o=jsonpath="{.items[$i].metadata.name}")
+# kubectl logs $(kubectl get pods -l app=http-log-client -o=jsonpath="{.items[$i].metadata.name}") > logs/$EXPERIMENT_NAME/latency/client-$i.log
+# done
 
-echo "deleting client..."
-kubectl delete -f $KUBERNETES_DIR/http-log-client.yml
+# echo "deleting client..."
+# kubectl delete -f $KUBERNETES_DIR/http-log-client.yml
 
-echo "deleting server..."
-kubectl delete -f $KUBERNETES_DIR/hermes-leader.yml
-kubectl delete -f $KUBERNETES_DIR/hermes-followers.yml
+# echo "deleting server..."
+# kubectl delete -f $KUBERNETES_DIR/hermes-leader.yml
+# kubectl delete -f $KUBERNETES_DIR/hermes-followers.yml
+
+echo "comparing data"
+diff logs/$EXPERIMENT_NAME/operations/leader.log logs/$EXPERIMENT_NAME/operations/follower-0.log > logs/$EXPERIMENT_NAME/operations/diff-leader-follower-0.diff
+diff logs/$EXPERIMENT_NAME/operations/leader.log logs/$EXPERIMENT_NAME/operations/follower-1.log > logs/$EXPERIMENT_NAME/operations/diff-leader-follower-1.diff
