e655565b1208542860e48cd7743fd681fe816f10
Lucas <lucas@prevision.com.br>
2022-06-15 15:06:21 -0300

    Add

--- scripts/ssh_hosts.sh
+++ scripts/ssh_hosts.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
-# ssh-keyscan -f ansible/known_hosts >> ~/.ssh/known_hosts
+emulabuser=$1
+known_hosts_file="ansible/known_hosts"
 
-gnome-terminal -e "ssh -p 22 LUCAS@pc443.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc484.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc515.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc441.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc445.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc494.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc447.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc460.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc444.emulab.net"
-gnome-terminal -e "ssh -p 22 LUCAS@pc448.emulab.net"
+ssh-keyscan -f $known_hosts_file >> ~/.ssh/known_hosts
 
-ssh -p 22 LUCAS@pc443.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc484.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc515.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc441.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc445.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc494.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc447.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc460.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc444.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
-ssh -p 22 LUCAS@pc448.emulab.net "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
+for line in $(cat $known_hosts_file); do
+    ssh -p 22 $emulabuser@$line "ifconfig | tr '\n' '~' | sed 's/~~/\n/g' | sed 's/~/ /g' | sed -E 's/^\s*(\S+)\s+.*\s+inet\s+(\S+)\s.*$/\1 \2/g'"
+done
