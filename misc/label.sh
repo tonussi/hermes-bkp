952b8f976581673c2dbb5a94786e23a9ad93e866
Lucas <lucas@prevision.com.br>
2022-06-19 15:20:08 -0300

    Add

--- misc/label.sh
+++ misc/label.sh
@@ -27,6 +27,12 @@ kubectl label nodes gke-hermes-default-pool-bdd746dd-hhlw role=client
 kubectl label nodes gke-hermes-default-pool-bdd746dd-n64v role=client
 kubectl label nodes gke-hermes-default-pool-bdd746dd-zq59 role=client
 
+kubectl label nodes gke-hermes-hermes-pool-341ebb48-fxj1 role=server
+kubectl label nodes gke-hermes-hermes-pool-341ebb48-r1vr role=server
+kubectl label nodes gke-hermes-hermes-pool-341ebb48-v1p5 role=server
+kubectl label nodes gke-hermes-hermes-pool-341ebb48-xntb role=client
+kubectl label nodes gke-hermes-hermes-pool-341ebb48-zkh7 role=client
+
 # k3d
 
 kubectl label nodes k3d-hermes-server-0 role=server --overwrite
